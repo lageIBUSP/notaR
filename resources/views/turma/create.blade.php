a79b7c95e4a68dfe17dd296192d23740158cc381
Oz <oz@guardian.albacora.com.br>
2021-01-19 18:39:39 -0300

    view básica criar turma

--- resources/views/turma/create.blade.php
+++ resources/views/turma/create.blade.php
@@ -12,7 +12,7 @@
 	<a class="navbar-brand" href="{{ URL::to('turmas') }}">turma Alert</a>
     </div>
     <ul class="nav navbar-nav">
-	<li><a href="{{ URL::to('turmas') }}">View All turmas</a></li>
+	<li><a href="{{ URL::to('turmas') }}">Ver todas as turmas</a></li>
 	<li><a href="{{ URL::to('turmas/create') }}">Create a turma</a>
     </ul>
 </nav>
@@ -20,7 +20,7 @@
 @section('content')
     <div class="container">
 	<div class="row">
-	    <h1>Submit a link</h1>
+	    <h1>Criar nova turma</h1>
 	</div>
 	<div class="row">
 	    <form action="/submit" method="post">
@@ -31,27 +31,20 @@
 		    </div>
 		@endif
 		<div class="form-group">
-		    <label for="title">Title</label>
-		    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
-		    @error('title')
+		    <label for="name">Nome</label>
+		    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Nome da turma" value="{{ old('nome') }}">
+		    @error('name')
 			<div class="invalid-feedback">{{ $message }}</div>
 		    @enderror
 		</div>
 		<div class="form-group">
-		    <label for="url">Url</label>
-		    <input type="text" class="form-control @error('url') is-invalid @enderror" id="url" name="url" placeholder="URL" value="{{ old('url') }}">
-		    @error('url')
-			<div class="invalid-feedback">{{ $message }}</div>
-		    @enderror
-		</div>
-		<div class="form-group">
-		    <label for="description">Description</label>
-		    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="description">{{ old('description') }}</textarea>
+		    <label for="description">Descrição</label>
+		    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="Classe da disciplina XYZ do ano ABCD">{{ old('description') }}</textarea>
 		    @error('description')
 			<div class="invalid-feedback">{{ $message }}</div>
 		    @enderror
 		</div>
-		<button type="submit" class="btn btn-primary">Submit</button>
+		<button type="submit" class="btn btn-primary">Criar</button>
 	    </form>
 	</div>
     </div>
