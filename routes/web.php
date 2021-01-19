a45bcb39308e92c8424fec01969e070f99514d04
Oz <oz@guardian.albacora.com.br>
2021-01-19 18:27:06 -0300

    first steps

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TurmaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::resources([
+    'users' => UserController::class,
+    'turmas' => TurmaController::class,
+]);
