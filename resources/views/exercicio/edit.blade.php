4d312859cc29e655039d674f42951b6d6f8ab420
Andre Chalom <andrechalom@gmail.com>
2021-03-06 15:45:54 -0300

    Update resources/views/exercicio/edit.blade.php

--- resources/views/exercicio/edit.blade.php
+++ resources/views/exercicio/edit.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container">
 	<div class="row">
-	    <h1>Criar novo exercício</h1>
+	    <h1>Alterar exercício</h1>
 	</div>
 	<div class="row">
 	    <form action="{{'/exercicio/'.$exercicio->id}}" method="POST">
