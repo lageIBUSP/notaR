3af0db9a4962ae0f5f8c74cb967132522710eeb6
Mali Oz Salles <malicsalles@gmail.com>
2021-04-16 16:27:36 -0300

    impedimentos MIGRATION

---
+++ app/Policies/ImpedimentoPolicy.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ImpedimentoPolicy
+{
+    use HandlesAuthorization;
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function view(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Impedimento  $impedimento
+     * @return mixed
+     */
+    public function edit(User $user, Impedimento $impedimento)
+    {
+        //
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Impedimento  $impedimento
+     * @return mixed
+     */
+    public function delete(User $user, Impedimento $impedimento)
+    {
+        return  $user->isAdmin();
+    }
+}
---
+++ database/factories/ImpedimentoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Impedimento;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ImpedimentoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Impedimento::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
