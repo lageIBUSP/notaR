e5f4c357e01ab2a4c171aa7683b96bebf6610b7d
Andre Chalom <andrechalom@gmail.com>
2021-05-23 16:42:01 -0300

    Novo home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.base')
 @section('content')
-	<h1>Bem vindo ao notaR<h1>
+	<h1>Bem vindo ao notaR</h1>
+	<p><em>Um sistema para notas automatizadas em cursos que utilizam a linguagem R.</em></p>
+
+	<p>O notaR é um sistema para correção automática de exercícios em linguagem R, armazenamento e 
+		visualização das notas resultantes. Ele é um software colaborativo, desenvolvido em linguagens de
+		código aberto.
+	</p>
+
+	<p> Se você está matriculado em um curso que está utilizando o notaR, seu professor deve encaminhar 
+		um nome de usuário e senha para você fazer login. Não esqueça de alterar a senha
+		após o primeiro login!
+	</p>
+
+	<p>	Caso contrário, você pode seguir a apostila do curso e completar os exercícios ao lado.
+	</p>
 @endsection
 
