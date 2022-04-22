b6279c0fd9fcb76fea7071cca51637d876b42de0
Mali Salles <malicsalles@gmail.com>
2022-04-22 18:17:16 -0300

    relatorios: refactor e export

--- resources/views/relatorio/notas.blade.php
+++ resources/views/relatorio/notas.blade.php
@@ -7,37 +7,7 @@
     @include('relatorio.select', ['turmas' => $turmas])
 
     @if ($turma ?? "")
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <td>
-                </td>
-                <!-- thead: nomes dos exercícios -->
-                @foreach($turma->prazos->sortBy('exercicio.name') as $key => $prazo)
-                    <td>
-                        {{$prazo->exercicio->name}}
-                    </td>
-                @endforeach 
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($turma->users->sortBy(['name','email']) as $key => $user)
-            <tr>
-                <!-- rownames: user names -->
-                <td>
-                    {{$user->name ? $user->name : $user->email}}
-                </td>
-                <!-- table body: notas válidas -->
-                @foreach($turma->prazos->sortBy('exercicio.name') as $key => $prazo)
-                    <td>
-                        {{$user->notaFinal($prazo)}}
-                    </td>
-                @endforeach 
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+        @include('relatorio.table', ['tabela' => $tabela])
     @endif
 
 
--- resources/views/relatorio/select.blade.php
+++ resources/views/relatorio/select.blade.php
@@ -6,9 +6,16 @@
                 <option value="{{$value->id}}" {{$value->id == (($turma ?? "") ? $turma->id : -1 ) ? "selected" : ""}}>{{$value->name}}</option>
             @endforeach
         </select>
+
         <label for="turma">Tipo de relatório:</label>
         <select id="tipo" name="tipo">
             <option value="notas" {{($tipo ?? '') == 'notas' ? "selected" : ""}}>Notas</option>
             <option value="realizacao" {{($tipo ?? '') == 'realizacao' ? "selected" : ""}}>Realização</option>
+        </select>
+
+        <label for="turma">Exportar como csv?</label>
+        <select id="export" name="export">
+            <option value="0" {{($export ?? '') == '0' ? "selected" : ""}}>Ver apenas</option>
+            <option value="1" {{($export ?? '') == '1' ? "selected" : ""}}>Exportar</option>
         </select>
     </form>
