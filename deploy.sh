cfcb37feb767e1d85f532f33ff23603c38bede69
Mali Oz <malicsalles@gmail.com>
2023-10-19 15:23:26 -0300

    updating docker syntax (!!! update server docker)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git pull
-docker-compose down
-docker-compose build
-docker-compose up -d
-docker exec -t notar_app_1 ./docker-deploy.sh
+docker compose down
+docker compose build
+docker compose up -d
+docker exec -t notar-app-1 ./docker-deploy.sh
