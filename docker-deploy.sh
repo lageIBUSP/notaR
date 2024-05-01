19e88fdca83e586e0eb3c35d3be13ec04bd6ec80
Mali Oz <malicsalles@gmail.com>
2024-05-01 14:27:53 -0300

    clear all caches

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -8,12 +8,11 @@ else
 fi
 # Makes sure our storage link is set up
 php artisan storage:link
-# Clear application and config caches
-php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
 # Runs any DB migration
 php artisan migrate --force
-# Setup caches
+# Clear and restart caches
+php artisan cache:clear
 php artisan config:cache
 php artisan view:cache
+php artisan route:cache
+php artisan event:cache
