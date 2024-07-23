f9eee6b3a9e707dbcfc7bf8b9b9a916e1e8b40af
Mali Oz <malicsalles@gmail.com>
2024-07-23 16:38:13 -0300

    default model sorting

--- app/Models/Topico.php
+++ app/Models/Topico.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Stephenjude\DefaultModelSorting\Traits\DefaultOrderBy;
 
 class Topico extends Model
 {
     use HasFactory;
     protected $table = 'topicos';
     protected $guarded = [];
+
+    /**
+     * Set a default ordering for this model
+     */
+    use DefaultOrderBy;
+    protected static $orderByColumn = 'order';
 
     // relationships
     public function exercicios()
