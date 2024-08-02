e9d475459d9e6930bb638a8cb9a195b2ca5bc79d
Mali Oz <malicsalles@gmail.com>
2024-08-02 17:17:58 -0300

    ne description for curso

--- database/migrations/2024_07_25_122648_create_cursos_table.php
+++ database/migrations/2024_07_25_122648_create_cursos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
             $table->timestamps();
         });
         Schema::table('turmas', function (Blueprint $table) {
